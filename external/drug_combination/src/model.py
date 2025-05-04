699adef4659e4e3971f2a4bc48387573e2d58e19
ja-nina <zukowskanina1@gmail.com>
2025-05-05 00:57:46 +0200

    simple nn model rewritten from tensorflow to pytorch

--- external/drug_combination/src/model.py
+++ external/drug_combination/src/model.py
@@ -1,73 +1,77 @@
-from keras.layers import Dense, BatchNormalization, Activation, Dropout
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR
+import torch.nn.functional as F
 from src import setting
-from keras.optimizers import Adam
-from keras import backend as K
-import tensorflow as tf
 
-class DrugsCombModel():
+class DrugsCombModel(nn.Module):
 
     def __init__(self, drug_a_features_len, drug_b_features_len, cl_features_len):
+        super(DrugsCombModel, self).__init__()
 
         self.drug_a_features_len = drug_a_features_len
         self.drug_b_features_len = drug_b_features_len
         self.cl_features_len = cl_features_len
-        self.input_len = int(drug_a_features_len) + int(drug_b_features_len) + int(cl_features_len)
+        self.input_len = drug_a_features_len + drug_b_features_len + cl_features_len
 
-    def __mlp_model(self, nodes_nums, input_len):
+        # Define layers here
+        self.layers = self._create_layers()
 
-        from keras.models import Sequential
-        model = Sequential()
+    def _create_layers(self):
+        layers = []
+        nodes_nums = setting.FC_layout
+        prev_nodes = self.input_len
 
-        for i in range(len(nodes_nums)):
+        for i, nodes in enumerate(nodes_nums):
+            layers.append(nn.Linear(prev_nodes, nodes))
+            layers.append(nn.BatchNorm1d(nodes))
+            layers.append(getattr(nn, setting.activation_method[0])())
+            layers.append(nn.Dropout(setting.dropout[i % len(setting.dropout)]))
+            prev_nodes = nodes
 
-            if i == 0:
-                model.add(Dense(nodes_nums[i], input_shape=(input_len,)))
-            else:
-                model.add(Dense(nodes_nums[i]))
+        layers.append(nn.Linear(prev_nodes, 1))  # Final output layer
+        return nn.ModuleList(layers)
 
-            model.add(BatchNormalization())
-            model.add(Activation(setting.activation_method[i % len(setting.activation_method)]))
-            model.add(Dropout(rate=setting.dropout[i % len(setting.dropout)]))
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
-        model.add(Dense(1))
-
-        return model
-
+    @staticmethod
     def correlation_coefficient_loss(y_true, y_pred):
-        x = y_true
-        y = y_pred
-        mx = K.mean(x)
-        my = K.mean(y)
-        xm, ym = x - mx, y - my
-        r_num = K.sum(tf.multiply(xm, ym))
-        r_den = K.sqrt(tf.multiply(K.sum(K.square(xm)), K.sum(K.square(ym))))
+        xm = y_true - y_true.mean()
+        ym = y_pred - y_pred.mean()
+        r_num = torch.sum(xm * ym)
+        r_den = torch.sqrt(torch.sum(xm ** 2) * torch.sum(ym ** 2))
         r = r_num / r_den
-
-        r = K.maximum(K.minimum(r, 1.0), -1.0)
-        return 1 - K.square(r)
+        r = torch.clamp(r, -1.0, 1.0)
+        return 1 - r ** 2
 
     @classmethod
     def get_loss(cls):
-
-        # select the loss function
+        # Select the loss function
         if setting.loss == 'pearson_correlation':
-            my_loss = cls.correlation_coefficient_loss
-
+            return cls.correlation_coefficient_loss
         else:
-            my_loss = 'mse'
-
-        return my_loss
+            return nn.MSELoss()
 
     @classmethod
     def compile_transfer_learning_model(cls, model):
+        # Learning rate scheduler and optimizer
+        optimizer = optim.Adam(model.parameters(), lr=setting.start_lr)
+        scheduler = ExponentialLR(optimizer, gamma=setting.lr_decay)
+        loss_fn = cls.get_loss()
 
-        custimized_rmsprop = Adam(lr=setting.start_lr, decay=setting.lr_decay)
-        model.compile(optimizer=custimized_rmsprop, loss=cls.get_loss(), metrics=['mse', cls.correlation_coefficient_loss])
-        return model
+        return model, optimizer, scheduler, loss_fn
 
-    def get_model(self, method = setting.model_type):
+    def get_model(self, method=setting.model_type):
+        return self
 
-        crispr_model = getattr(self, "_{!s}__{!s}_model".format(self.__class__.__name__, method), self.__mlp_model)(
-            setting.FC_layout, self.input_len)
-        return self.compile_transfer_learning_model(crispr_model)
-
+    # def summary(self, input_size):
+            # """
+            # Print a summary of the model.
+            # input_size: tuple of input tensor size (batch_size, channels, features)
+            # """
+            # from torchinfo import summary
+            # return summary(self, input_size==)
