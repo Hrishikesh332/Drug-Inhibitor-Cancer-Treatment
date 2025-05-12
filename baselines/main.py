8f2ba349f20333be11c3895e169c2a81edf58989
ja-nina <zukowskanina1@gmail.com>
2025-05-12 16:39:04 +0200

    added model, code for biomining

--- baselines/main.py
+++ baselines/main.py
@@ -2,12 +2,15 @@ from trans_synergy.models.trans_synergy.attention_main import setup_data, prepar
 from trans_synergy.data.trans_synergy_data import DataPreprocessor
 from trans_synergy.models.trans_synergy.attention_main import set_seed
 from baselines.traditional_ml import run_model
+from external.predicting_synergy_nn.src.utils.data_loader import CVDatasetHandler
 from tqdm import tqdm
+import argparse
 
 
-def run_transynergy():
+BASELINE_MODELS = ['random_forest', 'svm', 'decision_tree', 'ridge', 'knn']
+def run_transynergy(timeout = 120, n_iter = 15):
     
-    baseline_models = ['random_forest', 'svm', 'decision_tree']
+    baseline_models = BASELINE_MODELS
 
     _, X, Y, _, _ = setup_data()
 
@@ -21,7 +24,7 @@ def run_transynergy():
             'eval2': partition[4]
         }
 
-        # Dataloaders
+        # dataloaders, but we just extract csvs
         training_set, _, validation_set, test_set,  _ = prepare_splitted_datasets(partition_indices, Y.reshape(-1), X)
         X_train, y_train = training_set.data_cache, training_set.data_cache_y
         X_val, y_val = validation_set.data_cache, validation_set.data_cache_y
@@ -29,21 +32,91 @@ def run_transynergy():
         
         for baseline_model in baseline_models:
             run_model(
-                X_train,
-                y_train,
-                X_val=X_val,
-                y_val=y_val,
+                [X_train],
+                [y_train],
+                X_vals=[X_val],
+                y_vals=[y_val],
                 X_test=X_test,
                 y_test=y_test,
                 fold_idx=fold_idx,
                 model_name=baseline_model,
                 paper = "transynergy",
+                timeout = timeout,
+                n_iter = n_iter
             )   
 
 
-def run_biomining():
-    pass
+def run_biomining(fold: int = 1, n_iter = 15, timeout = 120):
+    handler = CVDatasetHandler(data_dir='external/predicting_synergy_nn/data', outer_fold=fold)
+    handler.setup()
+    baseline_models = BASELINE_MODELS
+
+    for baseline_model in tqdm(baseline_models):
+        X_trains = []
+        y_trains = []
+        X_vals = []
+        y_vals = []
+        
+        X_test, y_test =handler.get_dataset(type='test', fold=None)
+        
+        for inner_fold in range(1, 4):
+
+            X_train, y_train = handler.get_dataset(type='train', fold=inner_fold)
+            X_val, y_val = handler.get_dataset(type='val', fold=inner_fold)
+            
+            X_trains.append(X_train)
+            y_trains.append(y_train)    
+            X_vals.append(X_val)
+            y_vals.append(y_val)
+
+        run_model(
+                X_trains,
+                y_trains,
+                X_vals=X_vals,
+                y_vals=y_vals,
+                X_test=X_test,
+                y_test=y_test,
+                fold_idx=inner_fold,
+                model_name=baseline_model,
+                paper = "biomining",
+                timeout = timeout,
+                n_iter = n_iter
+            )      
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run TransSynergy or Biomining experiments.")
+    parser.add_argument(
+        "--mode",
+        type=str,
+        choices=["transynergy", "biomining"],
+        default="biomining",
+        help="Choose whether to run TransSynergy or Biomining."
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=15,
+        help="Number of iterations for hyperparameter tuning."
+    )
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        default=60,
+        help="Timeout for model training in seconds."
+    )
+    parser.add_argument(
+        "--fold",
+        type=int,
+        default=1,
+        help="Fold number for biomining (only applicable for biomining mode)."
+    )
+
+    args = parser.parse_args()
+
     set_seed(142)
-    run_transynergy()
+
+    if args.mode == "transynergy":
+        run_transynergy(timeout=args.timeout, n_iter=args.n_iter)
+    elif args.mode == "biomining":
+        run_biomining(fold=args.fold, timeout=args.timeout, n_iter=args.n_iter)
