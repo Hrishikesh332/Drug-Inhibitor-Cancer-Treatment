64588e860248b40da0ab2ee9458980af4d987c4e
ja-nina <zukowskanina1@gmail.com>
2025-05-19 17:06:18 +0200

    working anchors

--- explainability/anchors/config.py
+++ explainability/anchors/config.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
+from typing import Optional
 from explainability.explaination_config import ExplainationConfig
 
 
 @dataclass
 class AnchorConfig(ExplainationConfig):
     maximize: bool = True
-    num_explanations: int = 50
+    num_explanations: Optional[int] = None
     threshold: float = 0.9
     seed: int = 42
+    highest_quantile_for_binning: float = 0.85 # HYPERPARAM: Anchors do require categorical variables so we have to bin out target synergy score
+    lowest_quantile_for_binning: float = 0.15 # HYPERPARAM: Anchors do require categorical variables so we have to bin out target synergy score
