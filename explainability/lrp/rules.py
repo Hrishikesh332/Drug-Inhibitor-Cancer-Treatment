336dc0ebedbca5b06690d89544fb09c0544bf141
ja-nina <zukowskanina1@gmail.com>
2025-06-23 23:06:46 +0200

    gamma rule applied (as recommended by prof. Montavon)

--- explainability/lrp/rules.py
+++ explainability/lrp/rules.py
@@ -8,26 +8,9 @@ from zennit.canonizers import NamedMergeBatchNorm
 CompositeType = Composite 
 
 
-def lrp_rule_for_biomining(ctx, name, module): # inspired by https://iphome.hhi.de/samek/pdf/MonXAI19.pdf
+def lrp_rule_for_biomining(ctx, name, module):
     if isinstance(module, nn.Linear):
-        if name == "net.20": # LRP-0
-            return Epsilon(epsilon=0, zero_params=['bias'])
-        elif name == "net.16" or name == 'net.12' or name == 'net.8': # Lrp-epsilon 
-            return Epsilon(epsilon=1e-6, zero_params =['bias'])
-        elif  name == 'net.4': # Lrp-gamma
-            return Gamma(zero_params =['bias'])
-        elif name == "net.0": # a bit unsure about this one https://github.com/rodrigobdz/lrp/blob/2089fda5e74e1255ae062b6c8a0b281661690c75/zennit-lrp-tutorial.ipynb
-            low = torch.tensor([8.2900e-06, 8.2900e-06, 2.5200e-03, 2.5200e-03, 2.3800e-04, 2.3800e-04,
-        3.4200e-06, 3.4200e-06, 1.1800e-04, 1.1800e-04, 5.7900e-09, 5.7900e-09,
-        2.1800e-06, 2.1800e-06, 1.4300e-06, 1.4300e-06, 8.5900e-08, 8.5900e-08,
-        1.7200e-04, 1.7200e-04, 6.4200e-05, 6.4200e-05, 5.9700e-03, 5.9700e-03,
-        3.8800e-01, 3.8800e-01, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
-        0.0000e+00, 0.0000e+00, 0.0000e+00]).unsqueeze(0) # minimum values for each feature (on test dataset)
-            high = torch.tensor([1.0000, 1.0000, 0.9980, 0.9980, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000,
-        1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000,
-        1.0000, 1.0000, 1.0000, 1.0000, 0.9970, 0.9970, 1.0000, 1.0000, 1.0000,
-        1.0000, 1.0000, 1.0000, 2.0000, 1.0000, 2.0000]).unsqueeze(0) # maximum values for each feature (on test dataset)
-            return ZBox(low=low, high=high, zero_params =['bias'])
+        return Gamma(zero_params =['bias'])
     elif isinstance(module, nn.BatchNorm1d) or isinstance(module, nn.Tanh) or isinstance(module, nn.Dropout) or isinstance(module, nn.ReLU):  # for all other layers, e.g. BatchNorm1d
         # activations are ignore just as https://zennit.readthedocs.io/en/0.4.4/how-to/use-rules-composites-and-canonizers.html 
         return Pass()
